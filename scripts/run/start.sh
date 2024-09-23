2572a5a6f110f2267c36664d224ebc019b817280
Filype Rodrigues <lyperodrigues03@gmail.com>
2024-09-23 08:06:40 -0300

    rename docker container name

--- scripts/run/start.sh
+++ scripts/run/start.sh
@@ -4,9 +4,9 @@ VERSION=$1
 
 docker build -t "bancodehoras:$VERSION" --network=host "$PWD"
 
-docker rm -f bancodehoras
+docker rm -f bancodehoras-t2
 
-docker run -itd --name bancodehoras\
+docker run -itd --name bancodehoras-t2\
   -p "$PORT:8080"\
   --restart "always"\
   "bancodehoras:$VERSION"
